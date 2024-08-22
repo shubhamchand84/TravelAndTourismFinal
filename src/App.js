3242e8f2792e5d5503b79b6001c74fa057ab30e2
Web Crafters <webcrafters2024@gmail.com>
2024-08-22 10:29:10 +0530

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
-import Home from "./pages/Home/Home";
-import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home";
 import Header from "./components/Common/Header/Header";
 import Footer from "./components/Common/Footer/Footer";
-
 
 function App() {
   return (
@@ -12,7 +10,6 @@ function App() {
       <Header />
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/contact-us" element={<Contact />} />
       </Routes>
       <Footer />
     </>
